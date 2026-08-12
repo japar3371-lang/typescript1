e52c06706af26b4d38a8f1519c2ca51d6469e2c3
M.Raffadian pratama <japar3371@gmail.com>
2026-08-12 15:27:12 +0700

    Add files via upload

---
+++ bab0.ts
@@ -0,0 +1,49 @@
+console.log("hai semuanya");
+// console objeck yang mewakili "konsol" (trminal)
+// titik untuk mengakses sesuatu di dalam objek
+// log fungsi nya menampilkan log atau sesusatu
+// kurung buka dan tutup tempat kita memasukan apa yang kita inginkan diatampilkan
+// "hai semuanya" teks yang ingin ditampilkan  dengan (diapit dengan tnada kutip)
+console.log("Nama saya raffa");
+
+console.log("Saya sedang melajari pemograman typescript")
+
+
+console.log(47);
+console.log(3.14);
+console.log(300+400);
+console.log(200-30);
+console.log(200/30);
+
+let nama: string = "Raffa";
+let kelas: string ="x-RPL";
+let umur: string = "17 tahun";
+let sekolah: string = "SMK ANTARTIKA 1 SIDOARJO";
+
+console.log("Nama saya:" , nama);
+console.log("kelas:" ,kelas);
+console.log("umur:" ,umur);
+console.log("sekolah:" , sekolah);
+
+let Nama: string ="RAFFA";
+let nilai: Number = 80;
+console.log(`Siswa ${Nama} mendapat nilai ${nilai}`);
+//`= backstick sebagai pembungkus, bukan tanda kutip biasa
+//${}= untuk menyisipkan variable di dalam teks
+
+let Namasiswa: string = "Raffa";
+let Kelas: string = "x RPL";
+let nomorAbsen: number= 18;     
+let nilairatarata:number= 90.5;
+let KeteranganLulus:string="lulus";
+
+console.log("===========================");
+console.log("====DATA DIRI SAYA====");
+console.log(`Nama               : ${Namasiswa}`);
+console.log(`Kelas              : ${Kelas} `);
+console.log(`nomorAbsen         : ${nomorAbsen}`);
+console.log(`nilairatarata      : ${nilairatarata}`);
+console.log(`keteranganlulus    : ${KeteranganLulus}`);
+console.log("===========================");
+
+
---
+++ bab1.ts
@@ -0,0 +1,16 @@
+let namasiswa = "Raffa";
+let umur = "17";
+let sekolah = "Smk antartika 1 sidoarjo";
+
+console.log (namasiswa);
+console.log (umur);
+console.log (sekolah);
+// let kata kunci  yang memberitahukan Type script "saya ingin membuat variable baru"
+//nama siswa adalah merupakan suatu variable
+//"Raffa"nilai yang kita masukan ke dalam  ke variable
+// baris ini secara keseluruan artinya  "Buat variable bernama  namasiswa dan di isi dengan teks Raffa"
+//console.log() fungsi untuk menampilkan  output ke terminal 
+// namasiswa "nama variable  nama yang isinya ingin  kita tampilkan
+//Type script akan mencari variable  "namasiswa" ,mengambilnya nilinya ("Raffa") dan menampilkanya
+
+
